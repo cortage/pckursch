b653f9044d9d62bfeda462b19543a3eb9c8ec212
cortage <99875793+cortage@users.noreply.github.com>
2024-04-07 15:42:04 +0300

    initialization

---
+++ src/pages/p-k-s-a-i-t.js
@@ -0,0 +1,202 @@
+import "./p-k-s-a-i-t.css";
+
+const PKSAIT = () => {
+  return (
+    <div className="pksait">
+      <div className="desktop-1" />
+      <div className="basement">
+        <div className="basement1" />
+        <div className="basementlink">
+          <div className="wwwyoutubecomvolterpc">www.youtube.com/@volterpc</div>
+          <div className="tmevolterpcbot">t.me/volterpcbot</div>
+          <div className="vkcomvolterpc">vk.com/volterpc</div>
+          <img className="frame-icon" alt="" src="/frame3.svg" />
+          <img className="frame-icon1" alt="" src="/frame2.svg" />
+          <img className="frame-icon2" alt="" src="/frame.svg" />
+        </div>
+        <div className="contactbasement">
+          <b className="volterpcsupportgmailcom">volterpcsupport@gmail.com</b>
+          <b className="b">+7 (499) 000-00-00</b>
+          <b className="contactblockbasement">Контакты</b>
+        </div>
+        <div className="basementoglav">
+          <div className="mainbasement">Главная</div>
+          <div className="aboutbasement">О нас</div>
+          <div className="contactbasement1">Контакты</div>
+          <div className="popularbasement">Популярное</div>
+          <div className="categorybasement">Категории</div>
+        </div>
+      </div>
+      <b className="contact">
+        <p className="p">Контакты</p>
+        <ul className="volterpcsupportgmailcom1">
+          <li className="li">
+            <span>Работаем по городу Москва и Московской области</span>
+          </li>
+          <li className="li">
+            <span>+7 (499) 000-00-00</span>
+          </li>
+          <li>
+            <span>volterpcsupport@gmail.com</span>
+          </li>
+        </ul>
+      </b>
+      <div className="popular">
+        <div className="blockpcsell">
+          <b className="i5-10400fgeforce-rtx-3060ti1-container">
+            <p className="p">i5-10400F/GeForce RTX 3060ti/16gb/1tb ssd</p>
+            <p className="p1">45000 ₽</p>
+          </b>
+          <div className="blockpc" />
+          <b className="namepc">VolterPC lite</b>
+          <img className="pcpng-icon" alt="" src="/pcpng@2x.png" />
+        </div>
+        <div className="blockcomponentsell">
+          <b className="gbs-8gb-gddr6-container">
+            <p className="p">1770 МГц/</p>
+            <p className="p">14 gb/s/</p>
+            <p className="p">8gb GDDR6</p>
+            <p className="p1">35 000 ₽</p>
+          </b>
+          <div className="blockpc" />
+          <b className="geforce-rtx-3060">GeForce RTX 3060 Ti GAMING X</b>
+          <img
+            className="kv-3060-ti-gaming-1-icon"
+            alt=""
+            src="/kv3060tigaming-1@2x.png"
+          />
+        </div>
+        <div className="blockperipherysell">
+          <b className="characterperiph">
+            <p className="p">проводные/китай/пластик</p>
+            <p className="p1">900 ₽</p>
+          </b>
+          <img
+            className="blockperiphery-icon"
+            alt=""
+            src="/blockperiphery.svg"
+          />
+          <b className="eastvita">EastVita</b>
+          <img
+            className="headphonesell-icon"
+            alt=""
+            src="/headphonesell@2x.png"
+          />
+        </div>
+        <div className="popularitem">Популярные товары</div>
+      </div>
+      <div className="category">
+        <div className="blockcomponentsait">
+          <div className="blockcomponents1" />
+          <div className="parent">
+            <b className="b1">Комплектующие</b>
+            <img
+              className="componentpng-icon"
+              alt=""
+              src="/componentpng@2x.png"
+            />
+          </div>
+        </div>
+        <div className="blockpccategory">
+          <img
+            className="blockpccategory-icon"
+            alt=""
+            src="/blockpccategory.svg"
+          />
+          <div className="group">
+            <b className="b2">Готовые сборки</b>
+            <img className="pcpng-icon1" alt="" src="/pcpng@2x.png" />
+          </div>
+        </div>
+        <div className="blockperipherysait">
+          <img
+            className="blockpccategory-icon"
+            alt=""
+            src="/blockpccategory.svg"
+          />
+          <div className="perphcategory-parent">
+            <img
+              className="perphcategory-icon"
+              alt=""
+              src="/perphcategory@2x.png"
+            />
+            <b className="b3">Периферия</b>
+          </div>
+        </div>
+        <b className="categorytext">Категории</b>
+      </div>
+      <div className="aboutblock">
+        <div className="abouttext">
+          <p className="p">
+          Наш сайт, VolterPC, создан для того, чтобы сделать покупки компьютерной техники удобными и доступными для каждого. Мы предлагаем широкий выбор ПК, периферийных устройств и комплектующих, чтобы удовлетворить потребности как профессиональных пользователей, так и любителей.
+          </p>
+          <p className="p">&nbsp;</p>
+          <p className="p">
+          Мы стремимся предоставить вам лучший выбор, качество и сервис, чтобы ваше покупательное опыт был приятным и беззаботным.
+          </p>
+          <p className="p">
+          Добро пожаловать на VolterPC - ваш надежный партнер в мире компьютерной техники!
+          </p>
+        </div>
+        <b className="aboutdiv">О нас</b>
+      </div>
+      <div className="content">
+        <img className="advantages1-icon" alt="" src="/advantages1.png" />
+        <img className="advantages2-icon" alt="" src="/advantages2.png" />
+        <img className="advantages3-icon" alt="" src="/advantages3.png" />
+      </div>
+      <div className="mainabout">
+        <div className="welcomemain" />
+        <img
+          className="arrowdownoutline-icon"
+          alt=""
+          src="/arrowdownoutline.svg"
+        />
+        <img className="mainpcpng-icon" alt="" src="/mainpcpng@2x.png" />
+        <b className="welcomemain1">
+          <span>VOLTERPC</span>
+          <span className="span"> - ТВОЯ ЛУЧШАЯ СБОРКА</span>
+        </b>
+        <div className="welcometext">
+        Приветствуем вас на VolterPC! Мы рады предложить широкий ассортимент компьютеров, периферийных устройств и комплектующих высокого качества. Погрузитесь в мир передовых технологий с нами и обновите свой компьютер для новых достижений!
+        </div>
+        <div className="buttonpc">
+          <b className="wantpcbutton">хочу сборку</b>
+        </div>
+        <div className="buttonabout">
+          <b className="wantpcbutton">о нас</b>
+        </div>
+      </div>
+      <div className="header">
+        <div className="logo">
+          <div className="temporarynamelogo">VOLTERPC</div>
+          <img
+            className="temporarylogoback-icon"
+            alt=""
+            src="/temporarylogoback.svg"
+          />
+        </div>
+        <div className="navigation">
+          <div className="mainnav">Главная</div>
+          <div className="mainnav">О нас</div>
+          <div className="mainnav">Категории</div>
+          <div className="mainnav">Популярное</div>
+          <div className="contactnav">Контакты</div>
+        </div>
+        <div className="foruser">
+          <div className="searchblock">
+            <div className="searchmain" />
+            <div className="serachtext">Поиск</div>
+            <img className="searchpng-icon" alt="" src="/searchpng@2x.png" />
+          </div>
+          <div className="icons">
+            <img className="carticon" alt="" src="/carticon.svg" />
+            <img className="profileicon" alt="" src="/profileicon.svg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PKSAIT;
